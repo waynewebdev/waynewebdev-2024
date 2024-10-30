bfacb866f658cf73bb6c8bf106a13037f603cce9
Wayne McWilliams <waynemcwilliams@Waynes-MacBook-Pro.local>
2024-10-29 21:15:25 -0500

    Created the WayneWebDev theme

---
+++ wp-content/themes/waynewebdev/functions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * WayneWebDev functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package waynewebdev
+ */
+
+if (!defined('_S_VERSION')) {
+  // Replace the version number of the theme on each release.
+  //minor
+  //patch
+  define('_S_VERSION', '0.00.01');
+}
